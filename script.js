070b1a73465887ef69d2e876fbf8db865c022cfc
Ray C <raychanray64@gmail.com>
2022-09-18 14:53:42 -0400

    enabled grids to change color when mouse hover

--- script.js
+++ script.js
@@ -6,5 +6,6 @@ for (let i = 0; i < numOfgrid; i++)
 {
     const smallGrid = document.createElement("div");
     smallGrid.classList.add("smallGrids");
+    smallGrid.addEventListener("mouseover", () => smallGrid.classList.add("hover"));
     container.appendChild(smallGrid);
 }
