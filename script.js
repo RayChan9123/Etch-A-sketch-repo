257addcfedc7cf2fcfa6d4c5981a470ea7090323
Ray C <raychanray64@gmail.com>
2022-09-18 22:38:41 -0400

    added button to change grids size and clear grid

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 const container = document.querySelector(".container");
+const sizeBtn = document.querySelector("#sizeBtn");
+const clearBtn = document.querySelector("#clearBtn");
+const size = 16;//default grids num
 
-let numOfgrid = 16 * 16;
-
-for (let i = 0; i < numOfgrid; i++)
+function setupGrids(size) 
 {
-    const smallGrid = document.createElement("div");
-    smallGrid.classList.add("smallGrids");
-    smallGrid.addEventListener("mouseover", () => smallGrid.classList.add("hover"));
-    container.appendChild(smallGrid);
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++)
+    {
+        const smallGrid = document.createElement("div");
+        smallGrid.classList.add("smallGrids");
+        smallGrid.addEventListener("mouseover", () => smallGrid.classList.add("hover"));
+        container.appendChild(smallGrid);
+    }
+}
+
+function clearGrid() {
+    container.innerHTML = '';
+}
+
+function askSize(size) {
+    
+    size = prompt("What is the number of squares per side? 1-100","");
+    size > 100 ? size = 100:
+    size < 1 ? size = 1 : size; // in case input out of limit
+    clearGrid();
+    setupGrids(size);
+}
+
+
+clearBtn.addEventListener('click',clearGrid);///clearBtn.onclick = () => clearGrid();
+sizeBtn.addEventListener('click', askSize);
+
+window.onload = () => {
+    setupGrids(size);
 }
